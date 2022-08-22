f104a465946e72e11f45f3a027030acb8d684473
maxkellringer <max.kellringer@gmx.de>
2022-08-22 07:38:49 +0200

    Grammar

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>My React App</h1>>
+      <h1>My React App</h1>
       <h2>Author: Max Kellringer</h2>
       {/* <img src={} alt="profile-image" /> */}
     </div>
